84436ba91cbf5748d653c3715ba4cf04b1706b14
Don Juan Javier <townofdon@gmail.com>
2025-09-04 01:02:37 -0400

    get noise working with point mesh

--- scripts/meshgen.gd
+++ scripts/meshgen.gd
@@ -8,24 +8,45 @@ func initialize(props: Props) -> void:
 	props.on_changed.connect(generate)
 
 func generate(cfg: Config, noise: Noise) -> void:
-	if mesh is ImmediateMesh:
-		mesh.clear_surfaces()
-
+	march_cubes(cfg, noise)
 	debug_noise_grid(cfg, noise)
 	#hello_world()
+
+func march_cubes(cfg: Config, noise: Noise) -> void:
+	pass
 
 func debug_noise_grid(cfg: Config, noise: Noise) -> void:
 	mesh = ImmediateMesh.new()
 	mesh.surface_begin(Mesh.PrimitiveType.PRIMITIVE_POINTS)
-	var numCells = Vector3(
+	var numCells:Vector3i = Vector3i(
 		floor(cfg.room_width / cfg.cell_size),
 		floor(cfg.room_height / cfg.cell_size),
 		floor(cfg.room_depth / cfg.cell_size),
 	)
 
+	var min := INF
+	var max := -INF
+	var noise_samples:Array[float] = []
+	# first pass - sample all noise values in grid
 	for z in numCells.z:
 		for y in numCells.y:
 			for x in numCells.x:
+				var val := noise.get_noise_3d(x, y, z)
+				noise_samples.append(val)
+				if val < min: min = val
+				if val > max: max = val
+
+	# second pass - normalize noise values and create mesh
+	for z in numCells.z:
+		for y in numCells.y:
+			for x in numCells.x:
+				var i := x + y*numCells.x + z*numCells.y*numCells.x
+				var val:float = inverse_lerp(min, max, noise_samples.get(i))
+				var active := val >= cfg.noise_cutoff
+				if active:
+					mesh.surface_set_color(Color.AQUAMARINE)
+				else:
+					mesh.surface_set_color(Color.DARK_OLIVE_GREEN)
 				mesh.surface_add_vertex(Vector3(x * cfg.cell_size, y * cfg.cell_size, z * cfg.cell_size))
 
 	mesh.surface_end()
